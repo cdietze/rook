0b698daed6fa0d4107dc3f26c4699cb981f46e02
Christoph Dietze <christoph.dietze@kurzdigital.com>
2018-04-10 22:45:37 +0200

    Can select piece

--- core/src/main/java/rook/core/BoardState.java
+++ core/src/main/java/rook/core/BoardState.java
@@ -1,9 +1,11 @@
 package rook.core;
 
+import com.google.common.collect.Iterables;
 import pythagoras.i.Dimension;
 import pythagoras.i.IDimension;
 import pythagoras.i.IRectangle;
 import pythagoras.i.Rectangle;
+import react.IntValue;
 import react.RList;
 
 public class BoardState {
@@ -11,5 +13,14 @@ public class BoardState {
   public final IRectangle rect = new Rectangle(dim);
 
   public final RList<Piece> pieces = RList.create();
-}
+  /**
+   * The index of the currently selected piece in the [pieces] list.
+   * Or -1 if nothing is selected.
+   */
+  public final IntValue selectedPieceIndex = new IntValue(-1);
 
+  public int pieceIndexAtPos(final int pos) {
+    pieces.stream().filter(piece -> piece.pos.get() == pos).findFirst();
+    return Iterables.indexOf(pieces, piece -> piece.pos.get() == pos);
+  }
+}
