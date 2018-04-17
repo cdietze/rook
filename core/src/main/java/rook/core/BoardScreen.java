d60f06ccd0e3bd735f23dd2efebf8b695990aa25
Christoph Dietze <git@cdietze.de>
2018-04-17 22:36:26 +0200

    Rename Board.layer -> Board.rootLayer to disambiguate

--- core/src/main/java/rook/core/BoardScreen.java
+++ core/src/main/java/rook/core/BoardScreen.java
@@ -26,7 +26,7 @@ public class BoardScreen extends Screen {
     root.setSize(plat.graphics().viewSize);
     Board board = new Board(this);
     float margin = 10f;
-    ScaledElement scaledElement = new ScaledElement(board.layer);
+    ScaledElement scaledElement = new ScaledElement(board.rootLayer);
     scaledElement.addStyles(Style.BACKGROUND.is(Background.solid(Colors.BLACK).inset(margin)));
     root.add(scaledElement.setConstraint(BorderLayout.CENTER));
   }
