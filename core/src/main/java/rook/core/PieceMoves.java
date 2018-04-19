16a2b4d1573e78d0c28b3352ea438e2fb7e1d741
Christoph Dietze <git@cdietze.de>
2018-04-19 22:58:18 +0200

    Refactoring: rename `opponent` to `captures` because we may soon push pieces of our own side

--- core/src/main/java/rook/core/PieceMoves.java
+++ core/src/main/java/rook/core/PieceMoves.java
@@ -13,50 +13,50 @@ public class PieceMoves {
   /**
    * @returns a BitSet containing all possible destinations for the given piece.
    */
-  public static BitSet moves(IDimension dim, Piece.Type type, int pos, BitSet occupied, BitSet opponent, BitSet result) {
+  public static BitSet moves(IDimension dim, Piece.Type type, int pos, BitSet occupied, BitSet captures, BitSet result) {
     switch (type) {
       case BISHOP:
-        return bishopMoves(dim, pos, occupied, opponent, result);
+        return bishopMoves(dim, pos, occupied, captures, result);
       case KING:
-        return kingMoves(dim, pos, occupied, opponent, result);
+        return kingMoves(dim, pos, occupied, captures, result);
       case ROOK:
-        return rookMoves(dim, pos, occupied, opponent, result);
+        return rookMoves(dim, pos, occupied, captures, result);
     }
     throw new RuntimeException("unknown piece type: " + type);
   }
 
-  public static BitSet kingMoves(IDimension dim, int pos, BitSet occupied, BitSet opponent, BitSet result) {
+  public static BitSet kingMoves(IDimension dim, int pos, BitSet occupied, BitSet captures, BitSet result) {
     PointUtils.borderingNeighbors(dim, pos, result);
     result.andNot(occupied);
     return result;
   }
 
   @SuppressWarnings("Duplicates")
-  public static BitSet bishopMoves(IDimension dim, int pos, BitSet occupied, BitSet opponent, BitSet result) {
+  public static BitSet bishopMoves(IDimension dim, int pos, BitSet occupied, BitSet captures, BitSet result) {
     int x = toX(dim, pos);
     int y = toY(dim, pos);
-    slide(dim, x, y, UP_LEFT, occupied, opponent, result);
-    slide(dim, x, y, UP_RIGHT, occupied, opponent, result);
-    slide(dim, x, y, DOWN_RIGHT, occupied, opponent, result);
-    slide(dim, x, y, DOWN_LEFT, occupied, opponent, result);
+    slide(dim, x, y, UP_LEFT, occupied, captures, result);
+    slide(dim, x, y, UP_RIGHT, occupied, captures, result);
+    slide(dim, x, y, DOWN_RIGHT, occupied, captures, result);
+    slide(dim, x, y, DOWN_LEFT, occupied, captures, result);
     return result;
   }
 
   @SuppressWarnings("Duplicates")
-  public static BitSet rookMoves(IDimension dim, int pos, BitSet occupied, BitSet opponent, BitSet result) {
+  public static BitSet rookMoves(IDimension dim, int pos, BitSet occupied, BitSet captures, BitSet result) {
     int x = toX(dim, pos);
     int y = toY(dim, pos);
-    slide(dim, x, y, UP, occupied, opponent, result);
-    slide(dim, x, y, RIGHT, occupied, opponent, result);
-    slide(dim, x, y, DOWN, occupied, opponent, result);
-    slide(dim, x, y, LEFT, occupied, opponent, result);
+    slide(dim, x, y, UP, occupied, captures, result);
+    slide(dim, x, y, RIGHT, occupied, captures, result);
+    slide(dim, x, y, DOWN, occupied, captures, result);
+    slide(dim, x, y, LEFT, occupied, captures, result);
     return result;
   }
 
-  private static BitSet slide(IDimension dim, int x, int y, Direction dir, BitSet occupied, BitSet opponent, BitSet result) {
+  private static BitSet slide(IDimension dim, int x, int y, Direction dir, BitSet occupied, BitSet captures, BitSet result) {
     for (int i = 1; contains(dim, x + i * dir.x(), y + i * dir.y()); i++) {
       int p = toIndex(dim, x + i * dir.x(), y + i * dir.y());
-      if (opponent.get(p)) {
+      if (captures.get(p)) {
         result.set(p);
         break;
       }
@@ -70,13 +70,13 @@ public class PieceMoves {
    * @returns the index of the destination square when a piece on `pos` square moves (slides) in x,dirY up to `moveLength` times.
    * If the piece hits an impassable square on its way, the last passable square is returned.
    */
-  public static int slideInDir(IDimension dim, int pos, Direction dir, BitSet occupied, BitSet opponent, int moveLength) {
+  public static int slideInDir(IDimension dim, int pos, Direction dir, BitSet occupied, BitSet captures, int moveLength) {
     int curPos = pos;
     int posX = toX(dim, pos);
     int posY = toY(dim, pos);
     for (int i = 1; i <= moveLength && contains(dim, posX + i * dir.x(), posY + i * dir.y()); i++) {
       int p = toIndex(dim, posX + i * dir.x(), posY + i * dir.y());
-      if (opponent.get(p)) return p;
+      if (captures.get(p)) return p;
       if (occupied.get(p)) return curPos;
       else curPos = p;
     }
