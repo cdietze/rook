e07716e2fad5ba6eb2505d64cf8c803d1d6cfe6c
Christoph Dietze <christoph.dietze@kurzdigital.com>
2018-04-19 20:50:59 +0200

    Refactoring: Move MoveIntention into its own file

--- core/src/main/java/rook/core/GameState.java
+++ core/src/main/java/rook/core/GameState.java
@@ -1,6 +1,5 @@
 package rook.core;
 
-import com.google.common.base.MoreObjects;
 import com.google.common.collect.Iterables;
 import de.cdietze.playn_util.PointUtils;
 import playn.core.Log;
@@ -201,41 +200,5 @@ public class GameState {
     for (int i = set.nextSetBit(0); i >= 0; i = set.nextSetBit(i + 1)) {
       fogSquares.remove(i);
     }
-  }
-}
-
-class MoveIntention {
-
-  public final Piece piece;
-  public final IPoint dir;
-  public final int moveLength;
-
-  public final IntValue dest;
-
-  private final GameState state;
-
-  public MoveIntention(GameState state, Piece piece, IPoint dir, int moveLength) {
-    this.state = state;
-    this.piece = piece;
-    this.dir = dir;
-    this.moveLength = moveLength;
-    this.dest = new IntValue(calcDest());
-    state.occupiedSquaresForEnemy.connect(x -> this.calcDest());
-  }
-
-  private int calcDest() {
-    BitSet opponent = new BitSet();
-    state.playerPieces().forEach(p -> opponent.set(p.pos.get()));
-    return PieceMoves.slideInDir(state.dim, piece.pos.get(), dir.x(), dir.y(), state.occupiedSquaresForEnemy.get(), opponent, moveLength);
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-            .add("piece", piece)
-            .add("dir", dir)
-            .add("moveLength", moveLength)
-            .add("dest", dest.get())
-            .toString();
   }
 }
